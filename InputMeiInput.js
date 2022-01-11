5e8c819a18e1117750eba5e35f50d6eb84e41d2e
Deepak Yadav <deepakyadavdydy2@gmail.com>
2022-01-11 19:27:59 +0530

    small commit

---
+++ InputMeiInput.js
@@ -0,0 +1,10 @@
+var chu = prompt("Bhai Ek input de re de re")
+console.log("Tera jo input hai wo ye he : "+chu)
+var chut = prompt("Bhai Tu ye input " +chu+" ka kya karega ")
+console.log("Bhai ke bol : "+chut)
+var chute = prompt("Kr ke dekhaoo na bhai")
+console.log("Bhai ke bol : "+chute)
+console.log("Ye le Lo ")
+for(let i=1;i<=10;i++){
+    console.log(chu +" x "+ i + " = " + chu*i )
+}
---
+++ JavaUncleBachha.js
@@ -0,0 +1,18 @@
+console.log("Enter Your Name  Number :");
+        var d = prompt("Enter Your Name Number");
+        if(d>=1 &&  d<=18) console.log("Beta Sunno")
+        else if(d>=19 &&  d<=25) console.log("Bhaiya Sunno")
+        else if(d>=26 &&  d<=40) console.log("Uncle Sunno")
+        console.log("Table Of your Name number " + d+ " is")
+        for(let i=1;i<=10;i++){
+            console.log(d +" x "+ i + " = " + d*i )
+        }
+        console.log("Table Khatam Bhaisahab")
+        console.log("Uncle : Hum Bade chalu admi hai apke table ko ulta bhi kr skte hai Dekho Ge ?")
+        console.log("Beta : Dikhaye ")
+        console.log("Uncle : Toh ye lo phir ")
+        for(let i=10;i>0;i--){
+            console.log(d +" x "+ i + " = " + d*i )
+        }
+        console.log("Uncle : Dekha mera kamal ")
+        console.log("Beta be like : Hatt Chutiye...... ")
